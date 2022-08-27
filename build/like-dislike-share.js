6a0669626c8331b84e7c16a768b28fcb9a782cbc
Miguel Taciano <40744030+mtaciano@users.noreply.github.com>
2022-08-27 19:58:26 -0300

    Minor fix in source files

--- build/like-dislike-share.js
+++ build/like-dislike-share.js
@@ -3,9 +3,26 @@
 // @namespace   https://github.com/mtaciano
 // @match       https://www.youtube.com/*
 // @description "[" likes; "]" dislikes; "\" gets the current video link
-// @version     2.0.0
+// @version     2.0.1
 // @downloadURL https://raw.githubusercontent.com/mtaciano/monkey-scripts/main/build/like-dislike-share.js
 // @homepageURL https://github.com/mtaciano/monkey-scripts/
 // @grant       none
 // ==/UserScript==
-let a,b,c,d,e,f,g;async function h(){await g.click(),await g.click(),await new Promise(a=>setTimeout(a,600)),document.getElementById("copy-button")?.click(),navigator.clipboard.writeText(document.getElementById("share-url").value)}function i(){if(!/^\/watch/.test(location.pathname)){a=!1;return}a=!0,1==(b=document.getElementsByTagName("ytd-video-primary-info-renderer")).length?(d=(c=b[0].getElementsByTagName("button"))[0],e=c[1],g="Share"===c[2].getAttribute("aria-label")?c[2]:c[3]):(d=null,e=null,g=null)}i();new MutationObserver(i).observe(document.documentElement,{childList:!0,subtree:!0}),addEventListener("keypress",b=>{if(a)"true"!=b.target.getAttribute("contenteditable")&&"input"!=(f=b.target.tagName.toLowerCase())&&"textarea"!=f&&("BracketLeft"==b.code&&d?d.click():"BracketRight"==b.code&&e?e.click():"Backslash"==b.code&&g&&h())})
+let e, t, a, l, c, n, i;
+async function g() {
+    await i.click(), await i.click(), await new Promise((e)=>setTimeout(e, 600)), document.getElementById("copy-button")?.click(), navigator.clipboard.writeText(document.getElementById("share-url").value);
+}
+function r() {
+    if (!/^\/watch/.test(location.pathname)) {
+        e = !1;
+        return;
+    }
+    e = !0, 1 == (t = document.getElementsByTagName("ytd-video-primary-info-renderer")).length ? (l = (a = t[0].getElementsByTagName("button"))[0], c = a[1], i = "Share" === a[2].getAttribute("aria-label") ? a[2] : a[3]) : (l = null, c = null, i = null);
+}
+r();
+new MutationObserver(r).observe(document.documentElement, {
+    childList: !0,
+    subtree: !0
+}), addEventListener("keypress", (t)=>{
+    if (e) "true" != t.target.getAttribute("contenteditable") && "input" != (n = t.target.tagName.toLowerCase()) && "textarea" != n && ("BracketLeft" == t.code && l ? l.click() : "BracketRight" == t.code && c ? c.click() : "Backslash" == t.code && i && g());
+});
