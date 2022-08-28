ff9aafd50eb860fc11a90f092e46b9c36743d16b
Miguel Taciano <40744030+mtaciano@users.noreply.github.com>
2022-08-28 00:44:15 -0300

    Update like-dislike-share script

--- build/like-dislike-share.js
+++ build/like-dislike-share.js
@@ -3,26 +3,61 @@
 // @namespace   https://github.com/mtaciano
 // @match       https://www.youtube.com/*
 // @description "[" likes; "]" dislikes; "\" gets the current video link
-// @version     2.0.1
+// @version     2.1.0
 // @downloadURL https://raw.githubusercontent.com/mtaciano/monkey-scripts/main/build/like-dislike-share.js
 // @homepageURL https://github.com/mtaciano/monkey-scripts/
 // @grant       none
 // ==/UserScript==
-let e, t, a, l, c, n, i;
-async function g() {
-    await i.click(), await i.click(), await new Promise((e)=>setTimeout(e, 600)), document.getElementById("copy-button")?.click(), navigator.clipboard.writeText(document.getElementById("share-url").value);
+let e, t, n, l;
+function c(e) {
+    return new Promise((t)=>{
+        let n = document.getElementById(e);
+        if (n) {
+            t(n);
+            return;
+        }
+        let l = new MutationObserver((n)=>{
+            n.forEach((n)=>{
+                let c = Array.from(n.addedNodes).find((t)=>{
+                    let n = t;
+                    return n.parentElement?.id === e || n.id === e;
+                });
+                if (c) {
+                    t(c), l.disconnect();
+                    return;
+                }
+            });
+        });
+        l.observe(document.body, {
+            childList: !0,
+            subtree: !0
+        });
+    });
+}
+async function i(e) {
+    e.click();
+    let t = await c("close-button"), n = await c("copy-button");
+    t.click(), n.click(), navigator.clipboard.writeText(document.getElementById("share-url").value);
 }
 function r() {
     if (!/^\/watch/.test(location.pathname)) {
         e = !1;
         return;
     }
-    e = !0, 1 == (t = document.getElementsByTagName("ytd-video-primary-info-renderer")).length ? (l = (a = t[0].getElementsByTagName("button"))[0], c = a[1], i = "Share" === a[2].getAttribute("aria-label") ? a[2] : a[3]) : (l = null, c = null, i = null);
+    e = !0;
+    let c = document.getElementsByTagName("ytd-video-primary-info-renderer");
+    if (1 == c.length) {
+        let i = c[0].getElementsByTagName("button");
+        t = i[0], n = i[1], l = i[2];
+    } else t = null, n = null, l = null;
 }
 r();
-new MutationObserver(r).observe(document.documentElement, {
+const a = new MutationObserver(r);
+a.observe(document.documentElement, {
     childList: !0,
     subtree: !0
-}), addEventListener("keypress", (t)=>{
-    if (e) "true" != t.target.getAttribute("contenteditable") && "input" != (n = t.target.tagName.toLowerCase()) && "textarea" != n && ("BracketLeft" == t.code && l ? l.click() : "BracketRight" == t.code && c ? c.click() : "Backslash" == t.code && i && g());
+}), addEventListener("keypress", (c)=>{
+    if (!e || "true" == c.target.getAttribute("contenteditable")) return;
+    let r = c.target.tagName.toLowerCase();
+    "input" != r && "textarea" != r && ("BracketLeft" == c.code && t ? t.click() : "BracketRight" == c.code && n ? n.click() : "Backslash" == c.code && l && i(l));
 });
--- src/like-dislike-share.ts
+++ src/like-dislike-share.ts
@@ -3,75 +3,133 @@
 // @namespace   https://github.com/mtaciano
 // @match       https://www.youtube.com/*
 // @description "[" likes; "]" dislikes; "\" gets the current video link
-// @version     2.0.1
+// @version     2.1.0
 // @downloadURL https://raw.githubusercontent.com/mtaciano/monkey-scripts/main/build/like-dislike-share.js
 // @homepageURL https://github.com/mtaciano/monkey-scripts/
 // @grant       none
 // ==/UserScript==
 
-// TODO: refactor to TS style
-let onvideopage: any,
-  videoinfo,
-  buttons,
-  like: any,
-  dislike: any,
-  tag,
-  link: any;
+let onVideoPage: boolean;
+let likeButton: HTMLElement | null;
+let dislikeButton: HTMLElement | null;
+let shareButton: HTMLElement | null;
 
-async function getLink() {
-  // Create popup and wait for it to load
-  await link.click();
-  await link.click();
-  await new Promise((r) => setTimeout(r, 600));
+// Create a promise to wait for an element
+function waitForElementById(selector: string): Promise<HTMLElement> {
+  return new Promise((resolve) => {
+    // Already exists
+    const elem = document.getElementById(selector);
+    if (elem) {
+      resolve(elem);
+      return;
+    }
 
-  document.getElementById("copy-button")?.click();
+    // Does not exist, create observer
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        const elem = Array.from(mutation.addedNodes).find((node) => {
+          const elem = node as Element;
+          // Should return _true_ if its parent is the chosen one.
+          // This is because while testing only the children where visible
+          // so this is a workaround
+          if (elem.parentElement?.id === selector || elem.id === selector) {
+            return true;
+          } else {
+            return false;
+          }
+        }) as HTMLElement;
+
+        if (elem) {
+          resolve(elem);
+          observer.disconnect();
+          return;
+        }
+      });
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+// Get the link of the video
+async function getLink(popupButton: HTMLElement) {
+  // Open popup to create the shareable link
+  popupButton.click();
+
+  // Get the button to close the popup and copy the link
+  const closeButton = await waitForElementById("close-button");
+  const copyButton = await waitForElementById("copy-button");
+
+  // Close the popup and create the "link copied" popup
+  // BUG: for some reason if you click the close button it works once,
+  // after that it does not work anymore
+  closeButton.click();
+  copyButton.click();
+
+  // Copy to the clipboard
   navigator.clipboard.writeText(
     (document.getElementById("share-url") as HTMLInputElement).value
   );
 }
 
+// Find the buttons that are needed
 function findButtons() {
+  // Test if we are in a valid page
   if (!/^\/watch/.test(location.pathname)) {
-    onvideopage = false;
+    onVideoPage = false;
     return;
   }
-  onvideopage = true;
+  onVideoPage = true;
 
-  videoinfo = document.getElementsByTagName("ytd-video-primary-info-renderer");
-  if (videoinfo.length == 1) {
-    buttons = videoinfo[0].getElementsByTagName("button");
-    like = buttons[0];
-    dislike = buttons[1];
-    link = // Check if video has "applaud" button
-      buttons[2].getAttribute("aria-label") === "Share"
-        ? buttons[2]
-        : buttons[3];
+  const videoInfo = document.getElementsByTagName(
+    "ytd-video-primary-info-renderer"
+  );
+  if (videoInfo.length == 1) {
+    const buttons = videoInfo[0].getElementsByTagName("button");
+    likeButton = buttons[0];
+    dislikeButton = buttons[1];
+    shareButton = buttons[2];
+    // NOTE: It seems that the `applaud` button was changed to `thanks`
+    // which comes after `share`, so maybe there's no more need for this check
+    // link = // Check if video has `applaud` button
+    //   buttons[2].getAttribute("aria-label") === "Share"
+    //     ? buttons[2]
+    //     : buttons[3];
   } else {
-    like = null;
-    dislike = null;
-    link = null;
+    likeButton = null;
+    dislikeButton = null;
+    shareButton = null;
   }
 }
 
 findButtons();
-let observer = new MutationObserver(findButtons);
+const observer = new MutationObserver(findButtons);
 observer.observe(document.documentElement, { childList: true, subtree: true });
 
-// Add keybindings
-addEventListener("keypress", (e) => {
-  if (!onvideopage) return;
-
-  if ((e.target as HTMLElement).getAttribute("contenteditable") == "true")
+// Add event listener to the keys
+addEventListener("keypress", (event) => {
+  if (!onVideoPage) {
     return;
+  }
 
-  tag = (e.target as HTMLElement).tagName.toLowerCase();
-  if (tag == "input" || tag == "textarea") return;
+  // On your userpage, return
+  if ((event.target as HTMLElement).getAttribute("contenteditable") == "true") {
+    return;
+  }
 
-  if (e.code == "BracketLeft" && like) {
-    like.click();
-  } else if (e.code == "BracketRight" && dislike) {
-    dislike.click();
-  } else if (e.code == "Backslash" && link) {
-    getLink();
+  const tag = (event.target as HTMLElement).tagName.toLowerCase();
+  if (tag == "input" || tag == "textarea") {
+    return;
+  }
+
+  if (event.code == "BracketLeft" && likeButton) {
+    likeButton.click();
+  } else if (event.code == "BracketRight" && dislikeButton) {
+    dislikeButton.click();
+  } else if (event.code == "Backslash" && shareButton) {
+    getLink(shareButton);
   }
 });
